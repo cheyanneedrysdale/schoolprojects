cd63fb367b571a417f8a56ce9e7a1e96bb36ba98
cheyanne drysdale <cheyanneedrysdale@gmail.com>
2025-10-04 00:22:51 -0400

    realized i did not have a [required] field in Project.cs; added

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -3,6 +3,7 @@ using COMP2139_ICE.Models;
 using Microsoft.AspNetCore.Mvc;
 using COMP2139_ICE.Data;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
 namespace COMP2139_ICE.Controllers;
 
@@ -23,11 +24,21 @@ public class ProjectController : Controller
         var projects = _context.Projects.ToList();
         return View(projects);
     }
-    
 
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
 [HttpPost]
+[ValidateAntiForgeryToken]
 public IActionResult Create(Project project)
 {
+    if (!ModelState.IsValid)
+        return View(project);
+    
     _context.Projects.Add(project);
     _context.SaveChanges();
     return RedirectToAction("Index");
@@ -49,5 +60,58 @@ public IActionResult Details(int id)
 
     }
 
+[HttpGet]
+public IActionResult Edit(int? id)
+{
+    if (id == null) return NotFound();
+
+    var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id.Value);
+    if (project == null) return NotFound();
+
+    return View(project);
+
+}
+
+[HttpPost]
+[ValidateAntiForgeryToken]
+public IActionResult Edit(int id, Project project)
+{
+    if (id != project.ProjectId) return NotFound();
+    if (!ModelState.IsValid)
+    {
+        return View(project);
+    }
+
+    _context.Update(project);
+    _context.SaveChanges();
+    return RedirectToAction(nameof(Index));
+}
+
+[HttpGet]
+[ValidateAntiForgeryToken]
+public IActionResult Delete(int? id)
+{
+    if (id == null) return NotFound();
+
+    var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id.Value);
+    if (project == null) return NotFound();
+
+    return View(project);
+
+}
+
+[HttpPost]
+[ValidateAntiForgeryToken]
+public IActionResult DeleteConfirmed(int id)
+{
+    var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+    if (project != null)
+    {
+        _context.Projects.Remove(project);
+        _context.SaveChanges();
+    }
+
+    return RedirectToAction(nameof(Index));
+}
 
 }
